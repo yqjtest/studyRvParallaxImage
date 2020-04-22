c2920920d9617f356b654848294ad6164aa844bb
yqjtest <qijiang182@163.com>
2020-04-22 08:24:04 +0800

    项目初始化

---
+++ app/src/main/java/com/itat/mystudy01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.itat.mystudy01;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
